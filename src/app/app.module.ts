7ff1fa160433430e8f670373c31b84ff38fa2dcf
leticiareginajf <leticia.reginajf@ggmail.com>
2024-04-03 08:20:57 -0300

    Inline

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CardsModule } from './cards/cards.module';
+import { InlineComponent } from './inline/inline.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    InlineComponent,
     
   ],
   imports: [
---
+++ src/app/inline/inline.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inline',
+  template: `
+    <p>
+      inline works!
+    </p>
+  `,
+  styleUrl: './inline.component.css'
+})
+export class InlineComponent {
+
+}
