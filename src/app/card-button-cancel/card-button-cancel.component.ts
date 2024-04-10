075b84100493fb3bab7dbf22c8fda1eb77fdb80f
leticiareginajf <leticia.reginajf@gmail.com>
2024-04-09 21:01:06 -0300

    Analise de Css no Componente NG

--- src/app/card-button-cancel/card-button-cancel.component.ts
+++ src/app/card-button-cancel/card-button-cancel.component.ts
@@ -3,9 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-card-button-cancel',
   templateUrl: './card-button-cancel.component.html',
-
-  styleUrls: ['./card-button-cancel.component.css', '../card/card.component.css'],
+  styleUrls: ['./card-button-cancel.component.css'],
+  styles: [
+    '.card-cancel-button{
+      background-color: green,
+    }'
+  ]
 })
 export class CardButtonCancelComponent {
-
 }
