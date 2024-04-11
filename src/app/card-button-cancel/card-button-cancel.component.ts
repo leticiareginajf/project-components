59e99efb6bfff7b8249f55b4c05e20ba0845b548
leticiareginajf <leticia.reginajf@ggmail.com>
2024-04-11 08:00:45 -0300

    Analise Codigo Css

--- src/app/card-button-cancel/card-button-cancel.component.ts
+++ src/app/card-button-cancel/card-button-cancel.component.ts
@@ -1,14 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-card-button-cancel',
-  templateUrl: './card-button-cancel.component.html',
-  styleUrls: ['./card-button-cancel.component.css'],
-  styles: [
-    '.card-cancel-button{
-      background-color: green,
-    }'
-  ]
+    selector: 'app-card-button-cancel',
+    templateUrl: './card-button-cancel.component.html',
+    styleUrls: ['./card-button-cancel.component.css'],
+    styles: [ 'div.card-cancel-button{ background-color: green;}'],
 })
+
 export class CardButtonCancelComponent {
+
 }
